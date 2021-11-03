bde0082b04b0832d858d5f61b244ed9e61bd8860
priyak31 <63193469+priyak31@users.noreply.github.com>
2021-11-03 11:17:23 +0530

    Add files via upload

---
+++ newdemo/src/main/java/com/newpack/Book.java
@@ -0,0 +1,50 @@
+package com.newpack;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String author;
+    private BigDecimal price;
+	
+	public Book(String string, String string2, BigDecimal bigDecimal) {
+		// TODO Auto-generated constructor stub
+	}
+	public Long getId() {
+		return id;
+	}
+	public Book() {
+		super();
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+    //setters, getters, constructors...
+}
